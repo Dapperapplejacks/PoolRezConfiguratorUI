e8111c851c243dae64e75ec1273ed4ea0654b55f
Daniel Holt <djholt@live.unc.edu>
2024-04-12 21:07:14 -0400

    - Fixed up routes - Initial header changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { ConfiguratorComponent } from './configurator/configurator.component';
+import { TesterComponent } from './tester/tester.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent]
+    imports: [RouterOutlet, HeaderComponent, ConfiguratorComponent, TesterComponent]
 })
 export class AppComponent {
   title = 'PoolRezConfiguratorUI';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { ConfiguratorComponent } from './configurator/configurator.component';
+import { TesterComponent } from './tester/tester.component';
 
 export const routes: Routes = [
     { path: 'configurator', component: ConfiguratorComponent },
-    { path: '', component: ConfiguratorComponent },
+    { path: 'tester', component: TesterComponent },
+    { path: '', redirectTo: '/configurator', pathMatch: 'full' },
+    { path: '**', redirectTo: '/configurator'}
 ];
